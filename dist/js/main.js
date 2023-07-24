e8eaa7a040fb09a5a1f995e9c8109436c223afcd
Mohamed Nassar <mohammednassar740@gmail.com>
2023-07-24 06:10:22 +0300

    add search feature and edit some of logic

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,85 +1,73 @@
 "use strict";
 let themeBtn = document.getElementById("themeBtn");
 themeBtn.addEventListener("click", () => {
-    if (document.body.dataset.theme === "light") {
-        document.body.dataset.theme = "dark";
-        localStorage.setItem("theme", "dark");
-    }
-    else {
-        document.body.dataset.theme = "light";
-        localStorage.setItem("theme", "light");
-    }
-});
-if (!localStorage.getItem("theme")) {
+  if (document.body.dataset.theme === "light") {
+    document.body.dataset.theme = "dark";
+    localStorage.setItem("theme", "dark");
+  } else {
     document.body.dataset.theme = "light";
     localStorage.setItem("theme", "light");
-}
-else {
-    localStorage.getItem("theme") === "dark"
-        ? (document.body.dataset.theme = "dark")
-        : (document.body.dataset.theme = "light");
-}
-let searchBtn = document.getElementById("sreachBtn");
-let searchInput = document.getElementById("search");
-searchBtn.addEventListener("click", () => {
-    searchInput.classList.contains("active")
-        ? searchInput.classList.remove("active")
-        : searchInput.classList.add("active");
+  }
 });
+if (!localStorage.getItem("theme")) {
+  document.body.dataset.theme = "light";
+  localStorage.setItem("theme", "light");
+} else {
+  localStorage.getItem("theme") === "dark"
+    ? (document.body.dataset.theme = "dark")
+    : (document.body.dataset.theme = "light");
+}
 let runningItems = [];
 let completedItems = [];
 let runningElements = document.getElementById("runningItems");
-let completedElements = (document.getElementById("completedElements"));
+let completedElements = document.getElementById("completedElements");
 let createRunningItem = (itemTitle) => {
-    let date = new Date();
-    let item = {
-        title: itemTitle.toLowerCase(),
-        date: date.toDateString(),
-        time: date.toLocaleTimeString(),
-        finished: false,
-    };
-    return item;
+  let date = new Date();
+  let item = {
+    title: itemTitle.toLowerCase(),
+    date: date.toDateString(),
+    time: date.toLocaleTimeString(),
+    finished: false,
+  };
+  return item;
 };
 let createCompletedItem = (itemTitle) => {
-    let date = new Date();
-    let item = {
-        title: itemTitle.toLowerCase(),
-        date: date.toDateString(),
-        time: date.toLocaleTimeString(),
-        finished: true,
-    };
-    return item;
+  let date = new Date();
+  let item = {
+    title: itemTitle.toLowerCase(),
+    date: date.toDateString(),
+    time: date.toLocaleTimeString(),
+    finished: true,
+  };
+  return item;
 };
 let addToRunningArray = (itemTitle) => {
-    runningItems.push(createRunningItem(itemTitle));
+  runningItems.push(createRunningItem(itemTitle));
 };
 let addToCompletedArray = (itemTitle) => {
-    completedItems.push(createCompletedItem(itemTitle));
+  completedItems.push(createCompletedItem(itemTitle));
 };
-let addToElementParent = (item) => {
-    let article = document.createElement("article");
-    article.classList.add("item");
-    let leftSec = document.createElement("div");
-    leftSec.classList.add("left-sec");
-    let leftSpan = document.createElement("span");
-    if (item.finished === true) {
-        leftSpan.innerHTML = `<i class="f-icon fa-solid fa-check"></i>`;
-    }
-    leftSec.appendChild(leftSpan);
-    let rightSec = document.createElement("div");
-    rightSec.classList.add("right-sec");
-    let itemTitle = document.createElement("h3");
-    itemTitle.innerText = item.title;
-    let rightSpan = document.createElement("span");
-    rightSpan.innerText = `${item.date} at ${item.time}`;
-    rightSec.appendChild(itemTitle);
-    rightSec.appendChild(rightSpan);
-    article.appendChild(leftSec);
-    article.appendChild(rightSec);
-    if (item.finished === true)
-        completedElements.append(article);
-    else
-        runningElements.append(article);
+let addToElementParent = (item, parentElement) => {
+  let article = document.createElement("article");
+  article.classList.add("item");
+  let leftSec = document.createElement("div");
+  leftSec.classList.add("left-sec");
+  let leftSpan = document.createElement("span");
+  if (item.finished === true) {
+    leftSpan.innerHTML = `<i class="f-icon fa-solid fa-check"></i>`;
+  }
+  leftSec.appendChild(leftSpan);
+  let rightSec = document.createElement("div");
+  rightSec.classList.add("right-sec");
+  let itemTitle = document.createElement("h3");
+  itemTitle.innerText = item.title;
+  let rightSpan = document.createElement("span");
+  rightSpan.innerText = `${item.date} at ${item.time}`;
+  rightSec.appendChild(itemTitle);
+  rightSec.appendChild(rightSpan);
+  article.appendChild(leftSec);
+  article.appendChild(rightSec);
+  parentElement.append(article);
 };
 let form = document.getElementById("form");
 let submitBtn = document.getElementById("submitBtn");
@@ -89,78 +77,171 @@ let runningCounter = document.getElementById("runningCounter");
 let runningCounterNumber = 0;
 runningCounter.innerText = runningCounterNumber.toString();
 if (localStorage.getItem("runningItems")) {
-    runningItems = JSON.parse(localStorage.getItem("runningItems"));
-    runningCounterNumber = runningItems.length;
-    runningCounter.innerText = runningCounterNumber.toString();
-    runningItems.forEach((e) => {
-        addToElementParent(e);
-    });
+  runningItems = JSON.parse(localStorage.getItem("runningItems"));
+  runningCounterNumber = runningItems.length;
+  runningCounter.innerText = runningCounterNumber.toString();
+  runningItems.forEach((e) => {
+    addToElementParent(e, runningElements);
+  });
 }
 submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (input.value) {
-        errorSpan.classList.remove("active");
-        addToRunningArray(input.value);
-        addToElementParent(createRunningItem(input.value));
-        localStorage.setItem("runningItems", JSON.stringify(runningItems));
-        runningCounterNumber++;
-        runningCounter.innerText = runningCounterNumber.toString();
-        input.value = "";
-        window.location.reload();
-    }
-    else {
-        errorSpan.classList.add("active");
-    }
+  e.preventDefault();
+  if (input.value) {
+    errorSpan.classList.remove("active");
+    addToRunningArray(input.value);
+    addToElementParent(createRunningItem(input.value), runningElements);
+    localStorage.setItem("runningItems", JSON.stringify(runningItems));
+    runningCounterNumber++;
+    runningCounter.innerText = runningCounterNumber.toString();
+    input.value = "";
+    window.location.reload();
+  } else {
+    errorSpan.classList.add("active");
+  }
 });
-let completedCounter = (document.getElementById("completedCounter"));
+let completedCounter = document.getElementById("completedCounter");
 let completedCounterNumber = 0;
 completedCounter.innerText = completedCounterNumber.toString();
 let runningTasks = document.querySelectorAll("#runningItems .item");
 if (localStorage.getItem("completedItems")) {
-    completedItems = JSON.parse(localStorage.getItem("completedItems"));
-    completedCounterNumber = completedItems.length;
-    completedCounter.innerText = completedCounterNumber.toString();
-    completedItems.forEach((e) => {
-        addToElementParent(e);
-    });
+  completedItems = JSON.parse(localStorage.getItem("completedItems"));
+  completedCounterNumber = completedItems.length;
+  completedCounter.innerText = completedCounterNumber.toString();
+  completedItems.forEach((e) => {
+    addToElementParent(e, completedElements);
+  });
 }
 runningTasks = document.querySelectorAll("#runningItems .item");
-console.log(runningTasks);
 runningTasks.forEach((ele) => {
-    ele.addEventListener("click", (e) => {
-        let itemTitle = e.target.closest(".item").children[1].children[0].innerText;
-        ele.remove();
-        runningCounterNumber--;
-        runningCounter.innerText = runningCounterNumber.toString();
-        addToCompletedArray(itemTitle);
-        addToElementParent(createCompletedItem(itemTitle));
-        localStorage.setItem("completedItems", JSON.stringify(completedItems));
-        completedCounterNumber++;
-        completedCounter.innerText = completedCounterNumber.toString();
-        let x = 0;
-        runningItems.forEach((e) => {
-            if (e.title.toLowerCase() == itemTitle.toLowerCase()) {
-                runningItems.splice(x, 1);
-                localStorage.setItem("runningItems", JSON.stringify(runningItems));
-            }
-            else
-                x++;
-        });
+  ele.addEventListener("click", (e) => {
+    let itemTitle = e.target.closest(".item").children[1].children[0].innerText;
+    ele.remove();
+    runningCounterNumber--;
+    runningCounter.innerText = runningCounterNumber.toString();
+    addToCompletedArray(itemTitle);
+    addToElementParent(createCompletedItem(itemTitle), completedElements);
+    localStorage.setItem("completedItems", JSON.stringify(completedItems));
+    completedCounterNumber++;
+    completedCounter.innerText = completedCounterNumber.toString();
+    let x = 0;
+    runningItems.forEach((e) => {
+      if (e.title.toLowerCase() == itemTitle.toLowerCase()) {
+        runningItems.splice(x, 1);
+        localStorage.setItem("runningItems", JSON.stringify(runningItems));
+      } else x++;
     });
+  });
 });
 let allItemsBtn = document.getElementById("allItemsBtn");
 let runningItemsBtn = document.getElementById("runningItemsBtn");
 let completedItemsBtn = document.getElementById("completedItemsBtn");
 allItemsBtn?.addEventListener("click", () => {
-    localStorage.clear();
-    window.location.reload();
+  localStorage.removeItem("runningItems");
+  localStorage.removeItem("completedItems");
+  window.location.reload();
 });
 runningItemsBtn?.addEventListener("click", () => {
-    localStorage.removeItem("runningItems");
-    window.location.reload();
+  localStorage.removeItem("runningItems");
+  window.location.reload();
 });
 completedItemsBtn?.addEventListener("click", () => {
-    localStorage.removeItem("completedItems");
-    window.location.reload();
+  localStorage.removeItem("completedItems");
+  window.location.reload();
+});
+let searchBtn = document.getElementById("searchBtn");
+let searchInput = document.getElementById("search");
+let searchForm = document.getElementById("searchForm");
+let searchRunningElements = document.getElementById("searchRunningElements");
+let searchCompletedElements = document.getElementById(
+  "searchCompletedElements"
+);
+let searchMenu = document.getElementById("searchMenu");
+searchBtn.addEventListener("click", (e) => {
+  if (searchInput.classList.contains("active"))
+    searchInput.classList.remove("active");
+  else {
+    searchInput.classList.add("active");
+    searchInput.focus();
+  }
+  e.stopPropagation();
+});
+let runningAppearedItems = [];
+let completedAppearedItems = [];
+let overlay = document.querySelector(".overlay");
+let searchErrorSpan = document.getElementById("errorSpan");
+let runningDiv = document.querySelector(".menu .running");
+let completedDiv = document.querySelector(".menu .completed");
+searchForm.addEventListener("submit", (ele) => {
+  ele.preventDefault();
+  searchRunningElements.innerHTML = "";
+  runningAppearedItems.length = 0;
+  searchCompletedElements.innerHTML = "";
+  completedAppearedItems.length = 0;
+  runningItems.forEach((e) => {
+    if (e.title.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      searchMenu.classList.add("active");
+      overlay.classList.add("active");
+      if (runningAppearedItems.length === 0) {
+        runningAppearedItems.push(e);
+        addToElementParent(e, searchRunningElements);
+      } else {
+        for (let i = 0; i < runningAppearedItems.length; i++) {
+          if (
+            e.title === runningAppearedItems[i].title &&
+            e.date === runningAppearedItems[i].date &&
+            e.time === runningAppearedItems[i].time &&
+            e.finished === runningAppearedItems[i].finished
+          ) {
+          } else {
+            console.log("founded in the running list");
+            runningAppearedItems.push(e);
+            addToElementParent(e, searchRunningElements);
+          }
+        }
+      }
+    }
+  });
+  completedItems.forEach((e) => {
+    if (e.title.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      searchMenu.classList.add("active");
+      overlay.classList.add("active");
+      if (completedAppearedItems.length === 0) {
+        completedAppearedItems.push(e);
+        addToElementParent(e, searchCompletedElements);
+      } else {
+        for (let i = 0; i < completedAppearedItems.length; i++) {
+          if (
+            e.title === completedAppearedItems[i].title &&
+            e.date === completedAppearedItems[i].date &&
+            e.time === completedAppearedItems[i].time &&
+            e.finished === completedAppearedItems[i].finished
+          ) {
+          } else {
+            console.log("founded in the complete list");
+            completedAppearedItems.push(e);
+            addToElementParent(e, searchCompletedElements);
+          }
+        }
+      }
+    }
+  });
+  if (
+    searchRunningElements.innerHTML === "" &&
+    searchCompletedElements.innerHTML === ""
+  ) {
+    searchErrorSpan.style.display = "block";
+    runningDiv.style.display = "none";
+    completedDiv.style.display = "none";
+  } else {
+    searchErrorSpan.style.display = "none";
+    runningDiv.style.display = "block";
+    completedDiv.style.display = "block";
+  }
+});
+document.addEventListener("click", (e) => {
+  if (searchMenu.classList.contains("active") && e.target === overlay) {
+    searchMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  }
 });
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -20,15 +20,6 @@ if (!localStorage.getItem("theme")) {
     : (document.body.dataset.theme = "light");
 }
 //////////////////////////////////////////////////////////////////
-// search Button
-let searchBtn = <HTMLButtonElement>document.getElementById("sreachBtn");
-let searchInput = <HTMLInputElement>document.getElementById("search");
-searchBtn.addEventListener("click", (): void => {
-  searchInput.classList.contains("active")
-    ? searchInput.classList.remove("active")
-    : searchInput.classList.add("active");
-});
-//////////////////////////////////////////////////////////////////
 // item interface
 interface Item {
   // id: number;
@@ -80,7 +71,7 @@ let addToCompletedArray = (itemTitle: string): void => {
 };
 //////////////////////////////////////////////////////////////////
 // function to create a running items elements in HTML
-let addToElementParent = (item: Item): void => {
+let addToElementParent = (item: Item, parentElement: HTMLDivElement): void => {
   // create article
   let article = document.createElement("article");
   article.classList.add("item");
@@ -114,8 +105,9 @@ let addToElementParent = (item: Item): void => {
   article.appendChild(rightSec);
 
   // append element in the body
-  if (item.finished === true) completedElements.append(article);
-  else runningElements.append(article);
+  parentElement.append(article);
+  // if (item.finished === true) completedElements.append(article);
+  // else runningElements.append(article);
 };
 //////////////////////////////////////////////////////////////////
 // start running array and elements:-
@@ -133,7 +125,7 @@ if (localStorage.getItem("runningItems")) {
   runningCounterNumber = runningItems.length;
   runningCounter.innerText = runningCounterNumber.toString();
   runningItems.forEach((e: Item) => {
-    addToElementParent(e);
+    addToElementParent(e, runningElements);
   });
 }
 // add new elements
@@ -142,7 +134,7 @@ submitBtn.addEventListener("click", (e): void => {
   if (input.value) {
     errorSpan.classList.remove("active");
     addToRunningArray(input.value);
-    addToElementParent(createRunningItem(input.value));
+    addToElementParent(createRunningItem(input.value), runningElements);
     localStorage.setItem("runningItems", JSON.stringify(runningItems));
     runningCounterNumber++;
     runningCounter.innerText = runningCounterNumber.toString();
@@ -152,7 +144,6 @@ submitBtn.addEventListener("click", (e): void => {
     errorSpan.classList.add("active");
   }
 });
-
 //////////////////////////////////////////////////////////////////
 // start completed array and elements:-
 // get the new added item from form and add it to the running items array
@@ -168,12 +159,11 @@ if (localStorage.getItem("completedItems")) {
   completedCounterNumber = completedItems.length;
   completedCounter.innerText = completedCounterNumber.toString();
   completedItems.forEach((e: Item) => {
-    addToElementParent(e);
+    addToElementParent(e, completedElements);
   });
 }
 // add new completed elements
 runningTasks = document.querySelectorAll("#runningItems .item");
-console.log(runningTasks);
 runningTasks.forEach((ele) => {
   ele.addEventListener("click", (e) => {
     let itemTitle = e.target.closest(".item").children[1].children[0].innerText;
@@ -183,7 +173,7 @@ runningTasks.forEach((ele) => {
     runningCounter.innerText = runningCounterNumber.toString();
     // add new completed element to all places
     addToCompletedArray(itemTitle);
-    addToElementParent(createCompletedItem(itemTitle));
+    addToElementParent(createCompletedItem(itemTitle), completedElements);
     localStorage.setItem("completedItems", JSON.stringify(completedItems));
     completedCounterNumber++;
     completedCounter.innerText = completedCounterNumber.toString();
@@ -204,7 +194,8 @@ let runningItemsBtn = document.getElementById("runningItemsBtn");
 let completedItemsBtn = document.getElementById("completedItemsBtn");
 // remove all items from local storage
 allItemsBtn?.addEventListener("click", (): void => {
-  localStorage.clear();
+  localStorage.removeItem("runningItems");
+  localStorage.removeItem("completedItems");
   window.location.reload();
 });
 // remove running items from local storage
@@ -216,4 +207,117 @@ runningItemsBtn?.addEventListener("click", (): void => {
 completedItemsBtn?.addEventListener("click", (): void => {
   localStorage.removeItem("completedItems");
   window.location.reload();
+});
+//////////////////////////////////////////////////////////////////
+// search Button
+let searchBtn = <HTMLButtonElement>document.getElementById("searchBtn");
+let searchInput = <HTMLInputElement>document.getElementById("search");
+let searchForm = <HTMLFormElement>document.getElementById("searchForm");
+let searchRunningElements = <HTMLDivElement>(
+  document.getElementById("searchRunningElements")
+);
+let searchCompletedElements = <HTMLDivElement>(
+  document.getElementById("searchCompletedElements")
+);
+let searchMenu = <HTMLDivElement>document.getElementById("searchMenu");
+searchBtn.addEventListener("click", (e): void => {
+  if (searchInput.classList.contains("active"))
+    searchInput.classList.remove("active");
+  else {
+    searchInput.classList.add("active");
+    searchInput.focus();
+  }
+  e.stopPropagation();
+});
+// get elements based on the search input
+let runningAppearedItems: Item[] = [];
+let completedAppearedItems: Item[] = [];
+let overlay = <HTMLDivElement>document.querySelector(".overlay");
+let searchErrorSpan = <HTMLElement>document.getElementById("errorSpan");
+let runningDiv = <HTMLDivElement>document.querySelector(".menu .running");
+let completedDiv = <HTMLDivElement>document.querySelector(".menu .completed");
+searchForm.addEventListener("submit", (ele): void => {
+  ele.preventDefault();
+  // reset the elements
+  searchRunningElements.innerHTML = "";
+  runningAppearedItems.length = 0;
+  searchCompletedElements.innerHTML = "";
+  completedAppearedItems.length = 0;
+  // search running elements
+  runningItems.forEach((e: Item) => {
+    if (e.title.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      // active menu
+      searchMenu.classList.add("active");
+      overlay.classList.add("active");
+      // add element to the array and DOM
+      if (runningAppearedItems.length === 0) {
+        runningAppearedItems.push(e);
+        addToElementParent(e, searchRunningElements);
+      } else {
+        for (let i = 0; i < runningAppearedItems.length; i++) {
+          if (
+            e.title === runningAppearedItems[i].title &&
+            e.date === runningAppearedItems[i].date &&
+            e.time === runningAppearedItems[i].time &&
+            e.finished === runningAppearedItems[i].finished
+          ) {
+          } else {
+            console.log("founded in the running list");
+            runningAppearedItems.push(e);
+            addToElementParent(e, searchRunningElements);
+          }
+        }
+      }
+    }
+  });
+
+  // search completed elements
+  completedItems.forEach((e: Item) => {
+    if (e.title.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      // active menu
+      searchMenu.classList.add("active");
+      overlay.classList.add("active");
+      // add element to the array and DOM
+      if (completedAppearedItems.length === 0) {
+        completedAppearedItems.push(e);
+        addToElementParent(e, searchCompletedElements);
+      } else {
+        for (let i = 0; i < completedAppearedItems.length; i++) {
+          if (
+            e.title === completedAppearedItems[i].title &&
+            e.date === completedAppearedItems[i].date &&
+            e.time === completedAppearedItems[i].time &&
+            e.finished === completedAppearedItems[i].finished
+          ) {
+          } else {
+            console.log("founded in the complete list");
+            completedAppearedItems.push(e);
+            addToElementParent(e, searchCompletedElements);
+          }
+        }
+      }
+    }
+  });
+
+  // error message
+  if (
+    searchRunningElements.innerHTML === "" &&
+    searchCompletedElements.innerHTML === ""
+  ) {
+    searchErrorSpan.style.display = "block";
+    runningDiv.style.display = "none";
+    completedDiv.style.display = "none";
+  } else {
+    searchErrorSpan.style.display = "none";
+    runningDiv.style.display = "block";
+    completedDiv.style.display = "block";
+  }
+});
+
+// close the search menu with click in any place
+document.addEventListener("click", (e): void => {
+  if (searchMenu.classList.contains("active") && e.target === overlay) {
+    searchMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  }
 });
